04c36f1a77fb7f1b4528f9eef5a33d1ca38de695
Andrej Kažmirský <andrej.kazmirsky@student.tuke.sk>
2022-07-01 14:13:48 +0200

    Updated backend

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,31 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
+const { typeDefs, resolvers } = require("./src/index");
 
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
